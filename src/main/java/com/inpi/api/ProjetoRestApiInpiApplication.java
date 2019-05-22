99491cd5d06d6c5efc9243f7a73da9595c00b83c
Julio Lima <juliosergil@gmail.com>
2019-05-22 00:04:25 -0300

    Update ProjetoRestApiInpiApplication.java

--- src/main/java/com/inpi/api/ProjetoRestApiInpiApplication.java
+++ src/main/java/com/inpi/api/ProjetoRestApiInpiApplication.java
@@ -1,8 +1,22 @@
 package com.inpi.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EntityScan(basePackages = {"com.inpi.model"})
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.inpi.repository"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ProjetoRestApiInpiApplication {
 
